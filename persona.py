d13dc3670e4a8b982250862d6c603d6ee176bde4
Petr Vepřek <petr.veprek.cz420@gmail.com>
2016-02-18 23:10:31 +0000

    Work in progress

--- persona.py
+++ persona.py
@@ -13,47 +13,53 @@ PERSONAS = \
 {
     'geek': 
     {
-        'seedUrls': ['https://github.com/petrveprek/persona'],
-        'whiteList': None,
-        'blackList': None,
+        'spider':
+        {
+            'seedUrls': ['https://github.com/petrveprek/persona'],
+            'whiteList': None,
+            'blackList': ['fashion', 'politics'],
+        }
     }
 }
 
-class LinkParser(HTMLParser):
+class HtmlParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             for (key, value) in attrs:
                 if key == 'href':
                     link = urljoin(self.baseUrl, value)
                     self.links.append(link)
-    def get_links(self, url):
+    def get_text_links(self, url):
         self.baseUrl = url
+        self.text = ""
         self.links = []
         response = urlopen(url)
         if 'text/html' in response.getheader('Content-Type'):
             htmlBytes = response.read()
             htmlString = htmlBytes.decode('utf-8')
+            self.text = htmlString
             self.feed(htmlString)
-        return self.links
+        return self.text, self.links
 
-def crawl(seedUrls, maxVisits = None, direction = None):
+def crawl(seedUrls, whiteList, blackList, maxVisits = None, direction = None):
     urlsToVisit = deque(seedUrls)
     urlsVisited = set()
     if maxVisits == None:
         maxVisits = 10
     if direction == None:
         direction = 'breath-first'
-    parser = LinkParser()
+    parser = HtmlParser()
     while urlsToVisit and len(urlsVisited) < maxVisits:
         url = urlsToVisit.popleft()
-        links = parser.get_links(url)
+        text, links = parser.get_text_links(url)
         urlsVisited.add(url)
-        for link in links:
-            if link not in urlsVisited and link not in urlsToVisit:
-                if direction == 'depth-first':
-                    urlsToVisit.appendleft(link) # depth first
-                else:
-                    urlsToVisit.append(link) # breath first
+        if blackList == None or not any(black in text for black in blackList):
+            for link in links:
+                if link not in urlsVisited and link not in urlsToVisit:
+                    if direction == 'depth-first':
+                        urlsToVisit.appendleft(link) # depth first
+                    else:
+                        urlsToVisit.append(link) # breath first
         print("{} / {} {} {}x".format(len(urlsVisited), len(urlsToVisit), url, len(links)))
 
 def main():
@@ -62,7 +68,10 @@ def main():
     print("Started at {}".format(time.strftime("%Y-%m-%d %H:%M:%S")))
     started_at = time.time()
 
-    crawl(PERSONAS[PERSONA]['seedUrls'])
+    crawl(
+        PERSONAS[PERSONA]['spider']['seedUrls'],
+        PERSONAS[PERSONA]['spider']['whiteList'],
+        PERSONAS[PERSONA]['spider']['blackList'])
 
     print("Completed at {}".format(time.strftime("%Y-%m-%d %H:%M:%S")))
     elapsed = time.time() - started_at
